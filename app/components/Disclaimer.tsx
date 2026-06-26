3b7056d63901fe51a77d73206da23f8e9634a3f1
Carlos Guerrero <guerrerocarlos@gmail.com>
2026-06-26 17:48:19 -0300

    Remove Cloudflare Calls mentions

--- app/components/Disclaimer.tsx
+++ app/components/Disclaimer.tsx
@@ -1,31 +1,9 @@
 import type { FC } from 'react'
-import { cn } from '~/utils/style'
 
 interface DisclaimerProps {
 	className?: string
 }
 
-export const Disclaimer: FC<DisclaimerProps> = ({ className }) => {
-	return (
-		<p
-			className={cn(
-				'text-xs text-zinc-400 dark:text-zinc-500 max-w-prose',
-				className
-			)}
-		>
-			InglésConLiza Meet funciona con{' '}
-			<a className="underline" href="https://developers.cloudflare.com/calls/">
-				Cloudflare Calls
-			</a>
-			. Para crear tu propia aplicación WebRTC con Cloudflare Calls, empieza en
-			el{' '}
-			<a
-				className="underline"
-				href="https://dash.cloudflare.com/?to=/:account/calls"
-			>
-				panel de Cloudflare
-			</a>
-			.
-		</p>
-	)
+export const Disclaimer: FC<DisclaimerProps> = () => {
+	return null
 }
